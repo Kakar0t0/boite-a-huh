a50998e023bb944a89383b0c585400bebb327b8c
Kakar0t0 <maxime.mtz.pro@outlook.fr>
2024-01-23 18:57:23 +0100

    Initialisation

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+
+  async function playSound(soundFile) {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(soundFile);
+      await soundObject.playAsync();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Play Sound 1" onPress={() => playSound(require('./assets/soundfile1.wav'))} />
+      <Button title="Play Sound 2" onPress={() => playSound(require('./assets/soundfile2.wav'))} />
+      <Button title="Play Sound 3" onPress={() => playSound(require('./assets/soundfile3.wav'))} />
       <StatusBar style="auto" />
     </View>
   );
